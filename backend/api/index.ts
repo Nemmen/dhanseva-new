d579d23e6f85e80d34337c2a1c6c3fb4e648f40c
Nemmen <aryankammboz@gmail.com>
2025-12-23 13:10:45 +0530

    fixed

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,9 +1,9 @@
-const app = require('../dist/app.js');
-const { connectDatabase } = require('../dist/config/database.js');
-const { connectRedis } = require('../dist/config/redis.js');
+import app from '../src/app';
+import { connectDatabase } from '../src/config/database';
+import { connectRedis } from '../src/config/redis';
 
 // Initialize connections for serverless
 connectDatabase().catch(console.error);
 connectRedis().catch(console.error);
 
-module.exports = app.default || app;
+export default app;
